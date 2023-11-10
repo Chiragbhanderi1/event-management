fbad19d4b6cb69e4968ea3cf565a67b6ee845093
Chirag <cbhanderi666@gmail.com>
2023-11-10 23:16:34 +0530

    Initial commit

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use App\Models\Attendee;
+use App\Models\Event;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +23,12 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Gate::define("update-event", function ($user,Event $event) {
+        //     return $event->user->id === $user->id;
+        // });
+        // Gate::define('delete-attendee',function($user,Event $event,Attendee $attendee) {
+        //     return  $user->id ===$event->user->id ||
+        //         $user->id === $attendee->user_id;
+        // });       
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('events',EventController::class);
+Route::apiResource('events.attendee',AttendeeController::class)
+        ->scoped()->except('update');
+Route::post('/login',[ AuthController::class,'login']);
+Route::post('/logout',[ AuthController::class,'logout'])
+    ->middleware('auth:sanctum');
